3401dc14c7a5868542a734a67a0c1b4569b1aaa1
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-08-30 23:07:27 +0300

    Task 4 Sort Array

---
+++ Example004/Program.cs
@@ -0,0 +1,39 @@
+﻿// необязательная задача Написать программу сортировки массива от большего к меньшему. 
+//Массив задается размерностью N с клавиатуры, далее заполняется случайными целыми числами.
+int[] FillArray(int[] arr)
+{
+    Console.WriteLine("Введите элементы массива: ");
+    for(int i=0; i < arr.Length; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
+    return arr;
+}
+void ArrayMaxMin(int[] collection)
+{
+    for(int index = 0; index < collection.Length - 1; index++)
+    {
+        int maxPosition = index;
+        for(int j = index + 1; j < collection.Length; j++)
+        {
+            if(collection[j]>collection[maxPosition]) maxPosition = j;
+        }
+        int temporary = collection[index];
+        collection[index] = collection[maxPosition];
+        collection[maxPosition] = temporary;
+    }
+}
+try
+{
+    Console.WriteLine("Введите количество элементов массива:");
+    int count = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[count];
+    array = FillArray(array);
+    foreach (int i in array) Console.Write($"{i}, ");
+    Console.WriteLine();
+    ArrayMaxMin(array);
+    Console.Write("Отсортированный массив: ");
+    foreach (int i in array) Console.Write($"{i}, ");
+    Console.WriteLine();
+}
+catch
+{
+    Console.WriteLine("Введите только целые числа");
+}
