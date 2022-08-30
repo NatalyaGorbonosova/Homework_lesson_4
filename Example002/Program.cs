171d6ecc0a5f995dfe3478244faa675a80fe980f
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-08-30 22:13:45 +0300

    Task 2 SumNumber

---
+++ Example002/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumNumbers(string number)
+{
+    int n = number.Length;
+    int num = Convert.ToInt32(number);
+    int sum = 0;
+    for(int i = n - 1; i >= 0; i--) sum = sum + (num /Convert.ToInt32(Math.Pow(10, i)))%10;
+    return sum;
+}
+try
+{
+    Console.WriteLine("Введите целое число");
+    string str = Console.ReadLine();
+    int answer = SumNumbers(str);
+    Console.WriteLine($"Сумма цифр числа равна: {answer}");
+    
+}
+catch
+{
+    Console.WriteLine("Введите целое число");
+}
