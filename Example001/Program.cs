a62a9db69124821cfd878d4fd0020ab9503e04f1
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-08-30 18:31:30 +0300

    Task 1 Degree A^B

---
+++ Example001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int DegreeAandB(int number1, int number2)
+{
+    int deg = 1;
+    for(int i=1; i<=number2; i++)
+    {
+        deg = deg * number1;
+    }
+    return deg;
+}
+try
+{
+    Console.WriteLine("Введите два целых числа А и В");
+    int A = Convert.ToInt32(Console.ReadLine());
+    int B = Convert.ToInt32(Console.ReadLine());
+    int result = DegreeAandB(A, B);
+    Console.WriteLine($"А в степени В равно {result}");
+}
+catch
+{
+    Console.WriteLine("Введите только целые числа");
+}
